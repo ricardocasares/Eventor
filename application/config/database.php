bf9f6c5173eeb98688e18137406d13eefe8e244d
Ricardo Casares <ricardocasares@gmail.com>
2012-02-29 20:04:23 -0300

    Database settings

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'eventor';
+$db['default']['hostname'] = 'a.db.shared.orchestra.io';
+$db['default']['username'] = 'user_d792882f';
+$db['default']['password'] = 'JI@Klx6NswpNSu';
+$db['default']['database'] = 'db_d792882f';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
