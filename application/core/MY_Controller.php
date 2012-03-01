95bca5875465ddd00bb09cf8455d8a0a25112fe0
Ricardo Casares <ricardocasares@gmail.com>
2012-02-29 21:07:52 -0300

    Set output header in MY_Controller.php for no cache

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,6 +10,8 @@
 		public function __construct()
 		{
 			parent::__construct();
+			$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, no-transform, max-age=0, post-check=0, pre-check=0");
+			$this->output->set_header("Pragma: no-cache");
 			$this->_language();
 		}
 
