5e7f9a0e30ba5c9495daa1087e975baefb778340
Ricardo Casares <ricardocasares@gmail.com>
2012-02-29 21:03:05 -0300

    Disabled cookie_prefix on CI config.php

--- application/config/config.php
+++ application/config/config.php
@@ -265,8 +265,8 @@ $config['sess_time_to_update']	= 300;
 | 'cookie_secure' =  Cookies will only be set if a secure HTTPS connection exists.
 |
 */
-$config['cookie_prefix']	= "eventor-";
-$config['cookie_domain']	= ".eventor.orchestra.io";
+$config['cookie_prefix']	= "";
+$config['cookie_domain']	= "";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
