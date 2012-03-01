646070c166dc4b8b6c5913b57f4fa726f5469f21
Ricardo Casares <ricardocasares@gmail.com>
2012-03-01 02:25:07 -0300

    Added output headers to prevent caching, removed one header

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,7 +10,6 @@
 		public function __construct()
 		{
 			parent::__construct();
-			$this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', $last_update).' GMT');
 			$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
 			$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
 			$this->output->set_header("Pragma: no-cache");
