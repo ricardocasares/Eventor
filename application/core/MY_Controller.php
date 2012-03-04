d6c04aec0120cb40223f028b8bd465a957a8b0c3
Ricardo Casares <ricardocasares@gmail.com>
2012-03-04 07:29:09 -0300

    Desactivé el profiler

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -13,7 +13,6 @@
 			$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
 			$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
 			$this->output->set_header("Pragma: no-cache");
-			$this->output->enable_profiler(TRUE);
 			$this->data['settings'] = $this->settings = new Setting(1);
 			$this->_language();
 		}
